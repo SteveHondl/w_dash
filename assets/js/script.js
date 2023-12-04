2c69b827826188ad34625dd638790024b57d3ba8
Steve Hondl <steve.hondl@gmail.com>
2023-12-03 20:00:15 -0600

    finished html, added API key and console log checked to get current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
-var APIkey = "4a1b4ca6cde231990b27cb12f870fadc"
+$(document).ready(function () {
+var apiKey = `4a1b4ca6cde231990b27cb12f870fadc`;
+var apiUrl = `https://api.openweathermap.org/data/2.5/forecast`;
 
-
-document.getElementById('search-form').addEventListener('submit', function(event) {
+$('#search-bar').submit(function (event) {
     event.preventDefault();
-    
-    var cityName = document.getElementById('city-input').value.trim();
-    
+
+    var cityName = $('#city-input').val().trim();
+
     if (cityName !== '') {
-        console.log('City Name:', cityName);
-        
-    } else {
-        console.log('Please enter a city name.');
+      getWeatherData(cityName);
     }
+});
+
+function getWeatherData(cityName) {
+    var apiUrlWithParams = `${apiUrl}?q=${cityName}&appid=${apiKey}`;
+
+    // Use jQuery's AJAX to make the API request
+    $.ajax({
+      url: apiUrlWithParams,
+      method: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        // Log the data to the console
+        console.log('Weather Data:', data);
+
+        // TODO: Update the UI with the weather data
+      },
+      error: function (error) {
+        console.error('Error fetching data:', error);
+      }
+    });
+  }
 });
